347045282c1e27f3b87a18bda8d1b71e97d4d0c3
slowdata <slowmotion@outlook.com>
2019-10-06 00:24:26 +0100

    Starting part4

---
+++ part4/bloggit/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const blogRouter = require("./controllers/blog");
+
+const config = require("./utils/config");
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
