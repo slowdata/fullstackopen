6eefc3f98a32bcad384adf06f91ada469a5b4b86
slowdata <slowmotion@outlook.com>
2020-01-26 19:47:42 +0000

    Fix the log middleware with the bodyparser in wrong place

--- part4/bloggit/app.js
+++ part4/bloggit/app.js
@@ -4,15 +4,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const blogRouter = require("./controllers/blog");
+const { requestLogger } = require("./utils/middleware");
 
 const config = require("./utils/config");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.set("useUnifiedTopology", true);
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log("> Connected to MongoDB");
+  })
+  .catch(error => {
+    console.error("Problem connecting to MongoDB erro: " + error);
+  });
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(requestLogger);
+app.use(cors());
 
 app.use("/api/blogs", blogRouter);
 
